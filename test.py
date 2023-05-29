97c7fa1eea406e4115079419ce6ce4a1c5b1f6db
BanYa1126 <132338040+BanYa1126@users.noreply.github.com>
2023-05-30 01:29:57 +0900

    test.py
    이미지 크롤링

---
+++ test.py
@@ -0,0 +1,62 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
+import urllib.request
+import os
+
+def createDirectory(directory):
+    try:
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+    except OSError:
+        print("Error: Failed to create the directory.")
+
+def crawling_img(name):
+    driver = webdriver.Chrome()
+    driver.get("https://www.google.co.kr/imghp?hl=ko&tab=wi&authuser=0&ogbl")
+    elem = driver.find_element("name", "q")
+    elem.send_keys(name)
+    elem.send_keys(Keys.RETURN)
+
+    SCROLL_PAUSE_TIME = 1
+    # Get scroll height
+    last_height = driver.execute_script("return document.body.scrollHeight")  # 브라우저의 높이를 자바스크립트로 찾음
+    while True:
+        # Scroll down to bottom
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")  # 브라우저 끝까지 스크롤을 내림
+        # Wait to load page
+        time.sleep(SCROLL_PAUSE_TIME)
+        # Calculate new scroll height and compare with last scroll height
+        new_height = driver.execute_script("return document.body.scrollHeight")
+        if new_height == last_height:
+            try:
+                driver.find_element(By.CSS_SELECTOR,".mye4qd").click()
+            except:
+                break
+        last_height = new_height
+    
+    dir = ".\test" + "\\" + name
+    createDirectory(dir) #폴더 생성
+
+    images = driver.find_elements(By.CSS_SELECTOR,".rg_i.Q4LuWd")
+    count = 1
+    for image in images:
+        try:
+            image.click()
+            time.sleep(2)
+            imgUrl = driver.find_element(By.XPATH,'/html/body/div[2]/c-wiz/div[3]/div[2]/div[3]/div/div/div[3]/div[2]/c-wiz/div[1]/div[1]/div/div[2]/a/img').get_attribute("src")
+            opener= urllib.request.build_opener()
+            opener.addheaders=[('User-Agent','Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1941.0 Safari/537.36')]
+            urllib.request.install_opener(opener)
+            path = "C:\\Users\\paqgl\\PycharmProjects\\pythonProject_crawling\\bs4\\img\\" + name + "\\"
+            urllib.request.urlretrieve(imgUrl, str(count) + ".jpg")
+            count = count + 1
+        except:
+            pass
+
+    driver.close()
+
+books = ["책 이름"]
+for book in books:
+    crawling_img(book)
