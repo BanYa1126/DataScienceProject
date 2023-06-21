de9b1744d43ad5a277d05843773cb4d1db0eba4a
012050 <beomjunchoe@naver.com>
2023-06-21 16:21:19 +0900

    크롤러 개선

--- crawler.py
+++ crawler.py
@@ -13,6 +13,11 @@ import time
 def book_list(id, pw, ReturnData=1):
     # 전체 작동 시간 측정 시작부분
     start = time.time()
+    try:
+        os.mkdir("static/images/")
+        print("book_list: images 폴더 생성")
+    except:
+        print("book_list: images 폴더가 이미 존재합니다.")
 
     # try, except가 하나의 묶음
     # try 문에서 오류가 발생하면 크롬 드라이버가 없다는 오류 메시지를 출력하고 0을 반환
@@ -60,43 +65,20 @@ def book_list(id, pw, ReturnData=1):
         pass
 
     # 대출 기록 페이지로 이동
-    driver.get("https://lib.deu.ac.kr/lend_lend.mir")
+    # driver.get("https://lib.deu.ac.kr/lend_lend.mir")
+    driver.get("https://lib.deu.ac.kr/lend_mylist.mir")
     # driver.implicitly_wait(60)
+    time.sleep(3)
 
     bookdatalist = []
-    # 1 도서 등록번호 저장, 기본 값
+    # 1 책 이름, 2 저자
     if ReturnData == 1:
-        SeleniumBookList = driver.find_elements(By.CLASS_NAME, "text-center")
-        for datalist in SeleniumBookList[2::2]:
-            bookdatalist.append(datalist.text)
-    # 2 도서 이름, 저자 저장
-    elif ReturnData == 2:
-        SeleniumBookList = driver.find_elements(By.CLASS_NAME, "text-left")
-        for bookdata in SeleniumBookList[1:]:
-            bookdata = bookdata.text.split('/')
-            bookdatalist.append(bookdata)
-    # 3 도서 등록 번호, 도서 이름, 저자 저장
-    elif ReturnData == 3:
-        SeleniumBookList = driver.find_elements(By.CLASS_NAME, "text-center")
-        for datalist in SeleniumBookList[2::2]:
-            bookdatalist.append([datalist.text])
-        SeleniumBookList = driver.find_elements(By.CLASS_NAME, "text-left")
-        count = 0
-        for bookdata in SeleniumBookList[1:]:
-            bookdata = bookdata.text.split('/')
-            for BookNameOrWriter in bookdata:
-                bookdatalist[count].append(BookNameOrWriter)
-            count += 1
-    # 4 도서 등록 번호, 도서 이름
-    elif ReturnData == 4:
-        SeleniumBookList = driver.find_elements(By.CLASS_NAME, "text-center")
-        for datalist in SeleniumBookList[2::2]:
-            bookdatalist.append([datalist.text])
-        SeleniumBookList = driver.find_elements(By.CLASS_NAME, "text-left")
-        count = 0
-        for bookdata in SeleniumBookList[1:]:
-            bookdatalist[count].append(bookdata.text.split('/')[0])
-            count += 1
+        SeleniumBookList = driver.find_elements(By.CLASS_NAME, "text-left")[1:]
+        for onebook in SeleniumBookList:
+            tmp_list = onebook.text.split(" / ")
+            tmp_list.insert(0, "0")
+            bookdatalist.append(tmp_list)
+
     # 크롬 드라이버 종료
     driver.close()
     
